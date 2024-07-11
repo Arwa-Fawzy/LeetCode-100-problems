176a52a98ef90453b7b0edba5efc3abe02989238
Arwa Fawzy <101527083+Arwa-Fawzy@users.noreply.github.com>
2024-07-11 05:34:14 +0300

    Add files via upload

---
+++ Daily Temperatures.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+         stack<int>st; vector<int>res(temperatures.size(),0);
+        for(int i=0;i<temperatures.size();++i){
+            while(!st.empty()&&temperatures[i]>temperatures[st.top()]){
+                int index=st.top();
+                res[index]=i-index;
+                st.pop();
+            }st.push(i);
+        }
+        return res;
+    }
+};
