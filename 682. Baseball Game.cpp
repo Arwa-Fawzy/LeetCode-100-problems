7ec5a702a4ebef0b955904b31b22391ef2903f6e
Arwa Fawzy <101527083+Arwa-Fawzy@users.noreply.github.com>
2024-07-23 21:04:25 +0300

    Add files via upload

--- 682. Baseball Game.cpp
+++ 682. Baseball Game.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int calPoints(vector<string>& operations) {
-        stack <int>st; int ans=0;
-        for(int i=0; i<operations.size();++i){
-            if(operations[i]=="D"){
-                int x=2*st.top();
-                st.push(x);
-            }else if(operations[i]=="+"){
-                int x=st.top(); st.pop();
-            int y=x+st.top();
-                st.push(x);
-                st.push(y);
-
-            }else if(operations[i]=="C"){
+        std::stack <int>st; int sz=operations.size();
+        for(int i=0; i<sz; ++i){
+            if(operations[i]=="C"){
                 st.pop();
+            }else if(operations[i]=="D"){
+                st.push(st.top()*2);
+            }else if(operations[i]=="+"){
+                int f=st.top(), res;
+                st.pop();
+                res=f+st.top();
+                st.push(f);
+                st.push(res);
             }else{
-                st.push(stoi(operations[i]));
+                st.push(std::stoi(operations[i]));
             }
-        }while(!st.empty()){
+        }int ans=0;
+        while(!st.empty()){
             ans+=st.top();
             st.pop();
         }return ans;
