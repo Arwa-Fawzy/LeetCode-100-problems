1fdfd42e2c3e01a4dd2040508a1f48bbdfa005f8
Arwa Fawzy <101527083+Arwa-Fawzy@users.noreply.github.com>
2024-08-01 16:21:38 +0300

    Add files via upload

---
+++ 232. Implement Queue using Stacks.cpp
@@ -0,0 +1,46 @@
+class MyQueue {
+    stack<int>s1;
+    stack<int>s2;
+public:
+    
+    
+    void push(int x) {
+        s1.push(x);
+    }
+    
+    int pop() {
+        if(s2.empty()){
+            while(!s1.empty()){
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        int last=s2.top();
+        s2.pop();
+        return last;
+    }
+    
+    int peek() {
+        if(s2.empty()){
+            while(!s1.empty()){
+                s2.push(s1.top());
+                s1.pop();
+            }
+        }
+        int last=s2.top();
+        return last;
+    }
+    
+    bool empty() {
+        return s1.empty()&&s2.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
